d1c5aef5d23ae89f126ffaa55ea36cda3b23ab1d
Thomas Fankhauser <tommy@southdesign.de>
2016-07-04 12:38:27 +0200

    Added initiwl window with window type dock

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 CFLAGS=-I. -Wall
 DEPS = i3keys.h
 OBJS = main.o
-LIBS=-lm
+LIBS=-lX11
 
 all: i3keys
+	./i3keys
 
 %.o: %.c $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
--- main.c
+++ main.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+
+Display *display;
+Window window;
+int screen;
 
 int main() {
-  printf("Aloha\n");
+
+  // Open connection to X server
+  display = XOpenDisplay(NULL);
+  if (display == NULL) {
+    fprintf(stderr, "Cannot open display\n");
+    exit(1);
+  }
+  screen = DefaultScreen(display);
+
+  // Create window
+  window = XCreateSimpleWindow(display, RootWindow(display, screen), 0, 0, 800, 200, 0, None, WhitePixel(display, screen));
+
+  // Set window type to DOCK
+  Atom type = XInternAtom(display, "_NET_WM_WINDOW_TYPE", False);
+  Atom value = XInternAtom(display, "_NET_WM_WINDOW_TYPE_DOCK", False);
+  XChangeProperty(display, window, type, XA_ATOM, 32, PropModeReplace, (unsigned char *)&value, 1);
+
+  // Select kind of events we want to receive
+  XSelectInput(display, window, ExposureMask | KeyPressMask);
+
+  // Show the window
+  XMapWindow(display, window);
+
+  // Event loop
+  XEvent event;
+  while(1) {
+    XNextEvent(display, &event);
+
+    // Draw or redraw window
+    if(event.type == Expose) {
+      XFillRectangle(display, window, DefaultGC(display, screen), 20, 20, 10, 10);
+    }
+
+    if(event.type == KeyPress) {
+      break;
+    }
+  }
+
+  // Close connection to X server
+  XCloseDisplay(display);
+
   return 0;
 }
