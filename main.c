bc556cabc5107ac7ec91c483d4f714b749b88d37
Thomas Fankhauser <tommy@southdesign.de>
2016-07-09 19:48:42 +0200

    Refactored and cleaned up code.

--- main.c
+++ main.c
@@ -68,7 +68,7 @@ static void calculate_layout_dimensions() {
 
     // Calculate starting y
     y = key_margin + key->row * (key_margin + key_size);
-    
+
     // Set values to key layout
     key->x = x;
     key->y = y;
@@ -82,7 +82,7 @@ static void calculate_layout_dimensions() {
     if(x > total_keyboard_width) {
       total_keyboard_width = x;
     }
-    
+
     key++;
   }
 
@@ -91,7 +91,7 @@ static void calculate_layout_dimensions() {
 
   // Calculate offset from left to center keyboard
   keyboard_offset_from_left = (width - (total_keyboard_width + key_margin)) / 2;
-  
+
   // Apply offset to center the keyboard
   key = keys;
   while(key < keyEnd) {
@@ -121,7 +121,7 @@ static void render_keys() {
     int x = key->x + ((key->width - text_width) / 2);
     int y = key->y + ((key->height + font_height) / 2);
     XDrawString(display, window, gc, x, y, text, strlen(text));
-    
+
     key++;
   }
 }
@@ -132,19 +132,18 @@ static i3k_key* find_key_from_button(XButtonEvent *event) {
   i3k_key *keyEnd = keys + sizeof(keys)/sizeof(keys[0]);
   while(key < keyEnd) {
     if(
-       event->x >= key->x &&
-       event->x <= (key->x + key->width) &&
-       event->y >= key->y &&
-       event->y <= (key->y + key->height)
-    ){
+        event->x >= key->x &&
+        event->x <= (key->x + key->width) &&
+        event->y >= key->y &&
+        event->y <= (key->y + key->height)
+      ){
       return key;
     }
-    
+
     key++;
   }
   return NULL;
 }
-
 
 // Expose is called to draw and redraw
 static void expose(XEvent *event) {
@@ -176,12 +175,12 @@ static void send_key(int type, int keycode, int keymask) {
   event.keycode     = XKeysymToKeycode(display, keycode);
   event.state       = keymask;
   event.type        = type;
-// Send event
+  // Send event
   long event_mask = type == KeyPress ? KeyPressMask : KeyReleaseMask;
   XSendEvent(event.display, event.window, True, event_mask, (XEvent *)&event);
 }
 
-// button press is called on touch or click down
+// Button press is called on touch or click down
 static void button_press(XButtonEvent *event) {
   i3k_key *key = find_key_from_button(event);
   if(key == NULL) {
@@ -190,7 +189,7 @@ static void button_press(XButtonEvent *event) {
 
   // Handle modifier key press
   if(key->modifier_mask){
-    
+
     // Deselect potential last modifier key
     if(modifier_key != NULL && modifier_key != key){
       modifier_key->is_pressed = False;
@@ -210,12 +209,12 @@ static void button_press(XButtonEvent *event) {
       modifier_key = NULL;
     }
 
-  // Handle normal key press
+    // Handle normal key press
   } else {
     // Highlight key
     key->is_pressed = True;
     last_key = key;
-    
+
     // Send press to XServer
     send_key(KeyPress, key->keycode, modifier_mask);
   }
@@ -224,22 +223,116 @@ static void button_press(XButtonEvent *event) {
   render_keys();
 }
 
-// button release is called on touch up or release
+// Button release is called on touch up or release
 static void button_release(XButtonEvent *event) {
   i3k_key *key = last_key;
   if(last_key == NULL) {
     return;
   }
-  
+
   // Unhighlight key
   key->is_pressed = False;
-  
+
   // Send release to XServer
   send_key(KeyRelease, key->keycode, modifier_mask);
   last_key = NULL;
 
   // Rerender
   render_keys();
+}
+
+// Setup Colors
+static void setup_colors(){
+  // Colormap
+  colormap = DefaultColormap(display, 0);
+
+  // Colors
+  color_background = color_from_hex("#1B1D1E");
+  color_unfocused = color_from_hex("#303030");
+  color_unfocused_text = color_from_hex("#ffffff");
+  color_focused = color_from_hex("#00B7FF");
+  color_focused_text = color_from_hex("#ffffff");
+  
+  // Setup Graphics Context
+  gc = DefaultGC(display, screen);
+}
+
+// Setup Fonts
+static void setup_fonts(){
+  // Setup Font
+  font = XLoadQueryFont(display, key_font);
+  if (font == NULL) {
+    fprintf(stderr, "Cannot load font %s\n", key_font);
+    exit(1);
+  }
+  XSetFont(display, gc, font->fid);
+  font_height = font->ascent + font->descent;
+}
+
+// Setup Window
+static void setup_window(){
+  // Width is the full window width
+  width = DisplayWidth(display, screen);
+
+  // Calculate keyboard dimensions and layout for total_keyboard_height
+  calculate_layout_dimensions();
+
+  // Create window
+  window = XCreateSimpleWindow(display, RootWindow(display, screen), 0, 0, width, total_keyboard_height, 0, None, color_background.pixel);
+}
+
+// Setup Window Properties
+static void setup_window_properties(){
+  // Set window name
+  Atom type = XInternAtom(display, "_NET_WM_NAME", False);
+  Atom value = XInternAtom(display, "i3keys", False);
+  XChangeProperty(display, window, type, XA_ATOM, 32, PropModeReplace, (unsigned char *)&value, 1);
+
+  // Set window type to DOCK
+  type = XInternAtom(display, "_NET_WM_WINDOW_TYPE", False);
+  value = XInternAtom(display, "_NET_WM_WINDOW_TYPE_DOCK", False);
+  XChangeProperty(display, window, type, XA_ATOM, 32, PropModeReplace, (unsigned char *)&value, 1);
+
+  // Position at bottom of window
+  unsigned long strut[12];
+  memset(&strut, 0, sizeof(strut));
+  strut[3] = total_keyboard_height; // bottom
+  type = XInternAtom(display, "_NET_WM_STRUT_PARTIAL", False);
+  XChangeProperty(display, window, type, XA_CARDINAL, 32, PropModeReplace, (unsigned char *)strut, 12);
+  
+  // Show the window
+  XMapWindow(display, window);
+  XFlush(display);
+}
+
+// Start Event Loop
+static void start_event_loop(){
+  // Select kind of events we want to receive
+  XSelectInput(display, window, ExposureMask | ButtonPressMask | ButtonReleaseMask);
+
+  // Event loop
+  XEvent event;
+  while(1) {
+    XNextEvent(display, &event);
+
+    // Draw or redraw window
+    if(event.type == Expose) {
+      expose(&event);
+      continue;
+    }
+
+    // Handle mouse button press
+    if(event.type == ButtonPress) {
+      button_press((XButtonEvent*)&event);
+      continue;
+    }
+
+    // Handle mouse button release
+    if(event.type == ButtonRelease) {
+      button_release((XButtonEvent*)&event);
+      continue;
+    }
+  }
 }
 
 // Main
@@ -252,83 +345,15 @@ int main() {
     exit(1);
   }
   screen = DefaultScreen(display);
-  colormap = DefaultColormap(display, 0);
 
-  // Setup Colors
-  color_background = color_from_hex("#1B1D1E");
-  color_unfocused = color_from_hex("#303030");
-  color_unfocused_text = color_from_hex("#ffffff");
-  color_focused = color_from_hex("#00B7FF");
-  color_focused_text = color_from_hex("#ffffff");
-  
-  // Setup Graphics Context
-  gc = DefaultGC(display, screen);
+  // Setup
+  setup_colors();
+  setup_fonts();
+  setup_window();
+  setup_window_properties();
 
-  // Setup Font
-  font = XLoadQueryFont(display, key_font);
-  if (font == NULL) {
-    fprintf(stderr, "Cannot load font %s\n", key_font);
-    exit(1);
-  }
-  XSetFont(display, gc, font->fid);
-  font_height = font->ascent + font->descent;
-
-  // Width is the full window width
-  width = DisplayWidth(display, screen);
-
-  // Calculate keyboard dimensions and layout
-  calculate_layout_dimensions();
-
-  // Create window
-  window = XCreateSimpleWindow(display, RootWindow(display, screen), 0, 0, width, total_keyboard_height, 0, None, color_background.pixel);
-  
-  // Set window name
-  Atom type = XInternAtom(display, "_NET_WM_NAME", False);
-  Atom value = XInternAtom(display, "i3keys", False);
-  XChangeProperty(display, window, type, XA_ATOM, 32, PropModeReplace, (unsigned char *)&value, 1);
-
-  // Set window type to DOCK
-  type = XInternAtom(display, "_NET_WM_WINDOW_TYPE", False);
-  value = XInternAtom(display, "_NET_WM_WINDOW_TYPE_DOCK", False);
-  XChangeProperty(display, window, type, XA_ATOM, 32, PropModeReplace, (unsigned char *)&value, 1);
-    
-  // Position at bottom of window
-  unsigned long strut[12];
-  memset(&strut, 0, sizeof(strut));
-  strut[3] = total_keyboard_height; // bottom
-  type = XInternAtom(display, "_NET_WM_STRUT_PARTIAL", False);
-  XChangeProperty(display, window, type, XA_CARDINAL, 32, PropModeReplace, (unsigned char *)strut, 12);
-
-  // Select kind of events we want to receive
-  XSelectInput(display, window, ExposureMask | ButtonPressMask | ButtonReleaseMask);
-
-  // Show the window
-  XMapWindow(display, window);
-  XFlush(display);
-
-  // Event loop
-  XEvent event;
-  while(1) {
-    XNextEvent(display, &event);
-
-    // Draw or redraw window
-    if(event.type == Expose) {
-      expose(&event);
-      continue;
-    }
-    
-    // Handle mouse button press
-    if(event.type == ButtonPress) {
-      button_press((XButtonEvent*)&event);
-      continue;
-    }
-    
-    // Handle mouse button release
-    if(event.type == ButtonRelease) {
-      button_release((XButtonEvent*)&event);
-      continue;
-    }
-  }
+  // Loop
+  start_event_loop();
 
   // Close connection to X server
   XCloseDisplay(display);
